162d5ceae5ad1f0a699d2ef539de8e1026e27ef0
Victor Tran <vicr12345@gmail.com>
2020-06-08 08:32:02 +0000

    Translated using Weblate (Vietnamese)
    Currently translated at 100.0% (6 of 6 strings)
    
    Translation: the24/the24 Daemon
    Translate-URL: https://translate.vicr123.com/projects/the24/the24-daemon/vi/

--- daemon/translations/vi_VN.ts
+++ daemon/translations/vi_VN.ts
@@ -1,22 +1,22 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" sourcelanguage="en_US">
+<TS version="2.1" sourcelanguage="en_US" language="vi_VN">
 <context>
     <name>Stopwatch</name>
     <message>
         <location filename="../stopwatch.cpp" line="183"/>
         <source>the24</source>
-        <translation type="unfinished"></translation>
+        <translation>the24</translation>
     </message>
     <message>
         <location filename="../stopwatch.cpp" line="184"/>
         <source>Stopwatch</source>
-        <translation type="unfinished"></translation>
+        <translation>Bộ bấm giờ</translation>
     </message>
     <message>
         <location filename="../stopwatch.cpp" line="185"/>
         <source>A stopwatch was reset because the system clock was changed unexpectedly.</source>
-        <translation type="unfinished"></translation>
+        <translation>Một bộ bấm giờ bị đặt lại tại vì đồng hồ hệ thống bị thay đổi bất ngờ.</translation>
     </message>
 </context>
 <context>
@@ -24,17 +24,17 @@
     <message>
         <location filename="../timer.cpp" line="236"/>
         <source>the24</source>
-        <translation type="unfinished"></translation>
+        <translation>the24</translation>
     </message>
     <message>
         <location filename="../timer.cpp" line="237"/>
         <source>Time&apos;s up!</source>
-        <translation type="unfinished"></translation>
+        <translation>Hết giờ!</translation>
     </message>
     <message>
         <location filename="../timer.cpp" line="239"/>
         <source>Your timer has elapsed!</source>
-        <translation type="unfinished"></translation>
+        <translation>Bộ đếm ngược hoàn tất!</translation>
     </message>
 </context>
 </TS>
