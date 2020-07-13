ef9b4e0abafe27f3c9eaa8cd93cc6799dcd25d16
Oscar <blue@bluexguardian.com>
2020-07-13 13:50:40 +0000

    Translated using Weblate (Chinese (Simplified))
    Currently translated at 100.0% (63 of 63 strings)
    
    Translation: the24/the24 Desktop
    Translate-URL: https://translate.vicr123.com/projects/the24/the24-desktop/zh_Hans/

--- application/translations/zh_CN.ts
+++ application/translations/zh_CN.ts
@@ -1,22 +1,22 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" sourcelanguage="en_US">
+<TS version="2.1" sourcelanguage="en_US" language="zh_CN">
 <context>
     <name>AddClockPopover</name>
     <message>
         <location filename="../worldclock/addclockpopover.ui" line="35"/>
         <source>Add Clock</source>
-        <translation type="unfinished"></translation>
+        <translation>添加时钟</translation>
     </message>
     <message>
         <location filename="../worldclock/addclockpopover.ui" line="42"/>
         <source>What timezone do you want to add?</source>
-        <translation type="unfinished"></translation>
+        <translation>您要添加哪个时区？</translation>
     </message>
     <message>
         <location filename="../worldclock/addclockpopover.ui" line="71"/>
         <source>Search</source>
-        <translation type="unfinished"></translation>
+        <translation>搜索</translation>
     </message>
 </context>
 <context>
@@ -24,22 +24,22 @@
     <message>
         <location filename="../alarm/alarmpopover.ui" line="97"/>
         <source>Repeat on</source>
-        <translation type="unfinished"></translation>
+        <translation>重复开启</translation>
     </message>
     <message>
         <location filename="../alarm/alarmpopover.ui" line="135"/>
         <source>Add Alarm</source>
-        <translation type="unfinished"></translation>
+        <translation>添加闹钟</translation>
     </message>
     <message>
         <location filename="../alarm/alarmpopover.cpp" line="47"/>
         <source>Edit Alarm</source>
-        <translation type="unfinished"></translation>
+        <translation>修改闹钟</translation>
     </message>
     <message>
         <location filename="../alarm/alarmpopover.cpp" line="49"/>
         <source>New Alarm</source>
-        <translation type="unfinished"></translation>
+        <translation>新闹钟</translation>
     </message>
 </context>
 <context>
@@ -47,42 +47,42 @@
     <message>
         <location filename="../alarm/alarmwidget.cpp" line="111"/>
         <source>Repeats every day</source>
-        <translation type="unfinished"></translation>
+        <translation>每天重复</translation>
     </message>
     <message>
         <location filename="../alarm/alarmwidget.cpp" line="114"/>
         <source>Repeats on Weekdays</source>
-        <translation type="unfinished"></translation>
+        <translation>在工作日重复</translation>
     </message>
     <message>
         <location filename="../alarm/alarmwidget.cpp" line="117"/>
         <source>Repeats on Weekends</source>
-        <translation type="unfinished"></translation>
+        <translation>在周末重复</translation>
     </message>
     <message>
         <location filename="../alarm/alarmwidget.cpp" line="132"/>
         <source>Repeats on %1</source>
-        <translation type="unfinished"></translation>
+        <translation>在％1重复</translation>
     </message>
     <message>
         <location filename="../alarm/alarmwidget.cpp" line="146"/>
         <source>Snoozed until %1</source>
-        <translation type="unfinished"></translation>
+        <translation>延后至％1</translation>
     </message>
     <message>
         <location filename="../alarm/alarmwidget.cpp" line="160"/>
         <source>For this alarm</source>
-        <translation type="unfinished"></translation>
+        <translation>对于此闹钟</translation>
     </message>
     <message>
         <location filename="../alarm/alarmwidget.cpp" line="161"/>
         <source>Edit</source>
-        <translation type="unfinished"></translation>
+        <translation>编辑</translation>
     </message>
     <message>
         <location filename="../alarm/alarmwidget.cpp" line="170"/>
         <source>Remove</source>
-        <translation type="unfinished"></translation>
+        <translation>移除</translation>
     </message>
 </context>
 <context>
@@ -90,17 +90,17 @@
     <message>
         <location filename="../alarm/alarms.ui" line="100"/>
         <source>Add Alarm</source>
-        <translation type="unfinished"></translation>
+        <translation>添加闹钟</translation>
     </message>
     <message>
         <location filename="../alarm/alarms.ui" line="153"/>
         <source>Hmm...</source>
-        <translation type="unfinished"></translation>
+        <translation>嗯。。。</translation>
     </message>
     <message>
         <location filename="../alarm/alarms.ui" line="163"/>
         <source>Looks like the the24 service is not running, and alarms won&apos;t work without it.</source>
-        <translation type="unfinished"></translation>
+        <translation>the24服务未运行，没有它，闹钟将无法工作。</translation>
     </message>
 </context>
 <context>
@@ -109,7 +109,7 @@
         <location filename="../clockhelpers.cpp" line="38"/>
         <source>d</source>
         <comment>d for &quot;Day&quot; (1d)</comment>
-        <translation type="unfinished"></translation>
+        <translation>日</translation>
     </message>
 </context>
 <context>
@@ -117,57 +117,57 @@
     <message>
         <location filename="../mainwindow.ui" line="14"/>
         <source>the24</source>
-        <translation type="unfinished"></translation>
+        <translation>the24</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="94"/>
         <source>World Clock</source>
-        <translation type="unfinished"></translation>
+        <translation>世界时间</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="120"/>
         <source>Alarms</source>
-        <translation type="unfinished"></translation>
+        <translation>闹钟</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="143"/>
         <source>Stopwatch</source>
-        <translation type="unfinished"></translation>
+        <translation>跑表</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="166"/>
         <source>Timer</source>
-        <translation type="unfinished"></translation>
+        <translation>计时器</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="219"/>
         <source>File Bug</source>
-        <translation type="unfinished"></translation>
+        <translation>文件报错</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="228"/>
         <source>Sources</source>
-        <translation type="unfinished"></translation>
+        <translation>来源</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="237"/>
         <source>About</source>
-        <translation type="unfinished"></translation>
+        <translation>关于</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="246"/>
         <source>Exit</source>
-        <translation type="unfinished"></translation>
+        <translation>退出</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="249"/>
         <source>Ctrl+Q</source>
-        <translation type="unfinished"></translation>
+        <translation>Ctrl+Q</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="51"/>
         <source>Help</source>
-        <translation type="unfinished"></translation>
+        <translation>帮助</translation>
     </message>
 </context>
 <context>
@@ -175,12 +175,12 @@
     <message>
         <location filename="../timer/newtimerpopover.ui" line="35"/>
         <source>Add Timer</source>
-        <translation type="unfinished"></translation>
+        <translation>添加计时器</translation>
     </message>
     <message>
         <location filename="../timer/newtimerpopover.ui" line="45"/>
         <source>Start Timer</source>
-        <translation type="unfinished"></translation>
+        <translation>启动计时器</translation>
     </message>
 </context>
 <context>
@@ -188,17 +188,17 @@
     <message>
         <location filename="../stopwatch/stopwatch.ui" line="100"/>
         <source>Start Stopwatch</source>
-        <translation type="unfinished"></translation>
+        <translation>启动秒表</translation>
     </message>
     <message>
         <location filename="../stopwatch/stopwatch.ui" line="153"/>
         <source>Hmm...</source>
-        <translation type="unfinished"></translation>
+        <translation>嗯。。。</translation>
     </message>
     <message>
         <location filename="../stopwatch/stopwatch.ui" line="163"/>
         <source>Looks like the the24 service is not running, and stopwatches won&apos;t work without it.</source>
-        <translation type="unfinished"></translation>
+        <translation>the24服务未运行，没有它，秒表将无法工作。</translation>
     </message>
 </context>
 <context>
@@ -206,12 +206,12 @@
     <message>
         <location filename="../stopwatch/stopwatchwidget.ui" line="74"/>
         <source>Reset</source>
-        <translation type="unfinished"></translation>
+        <translation>重置</translation>
     </message>
     <message>
         <location filename="../stopwatch/stopwatchwidget.ui" line="98"/>
         <source>Remove</source>
-        <translation type="unfinished"></translation>
+        <translation>移除</translation>
     </message>
 </context>
 <context>
@@ -219,77 +219,77 @@
     <message>
         <location filename="../timer/timer.ui" line="100"/>
         <source>Add Timer</source>
-        <translation type="unfinished"></translation>
+        <translation>添加计时器</translation>
     </message>
     <message>
         <location filename="../timer/timer.ui" line="156"/>
         <source>Start a timer</source>
-        <translation type="unfinished"></translation>
+        <translation>启动计时器</translation>
     </message>
     <message>
         <location filename="../timer/timer.ui" line="169"/>
         <source>TextLabel</source>
-        <translation type="unfinished"></translation>
+        <translation>文字标签</translation>
     </message>
     <message>
         <location filename="../timer/timer.ui" line="203"/>
         <source>15 min</source>
-        <translation type="unfinished"></translation>
+        <translation>15 分钟</translation>
     </message>
     <message>
         <location filename="../timer/timer.ui" line="210"/>
         <source>2 hr</source>
-        <translation type="unfinished"></translation>
+        <translation>2 小时</translation>
     </message>
     <message>
         <location filename="../timer/timer.ui" line="224"/>
         <source>1 hr</source>
-        <translation type="unfinished"></translation>
+        <translation>1 小时</translation>
     </message>
     <message>
         <location filename="../timer/timer.ui" line="231"/>
         <source>1 min</source>
-        <translation type="unfinished"></translation>
+        <translation>1 分钟</translation>
     </message>
     <message>
         <location filename="../timer/timer.ui" line="238"/>
         <source>2 min</source>
-        <translation type="unfinished"></translation>
+        <translation>2 分钟</translation>
     </message>
     <message>
         <location filename="../timer/timer.ui" line="245"/>
         <source>30 min</source>
-        <translation type="unfinished"></translation>
+        <translation>30 分钟</translation>
     </message>
     <message>
         <location filename="../timer/timer.ui" line="252"/>
         <source>5 min</source>
-        <translation type="unfinished"></translation>
+        <translation>5 分钟</translation>
     </message>
     <message>
         <location filename="../timer/timer.ui" line="259"/>
         <source>10 min</source>
-        <translation type="unfinished"></translation>
+        <translation>10 分钟</translation>
     </message>
     <message>
         <location filename="../timer/timer.ui" line="217"/>
         <source>Something Else</source>
-        <translation type="unfinished"></translation>
+        <translation>其他</translation>
     </message>
     <message>
         <location filename="../timer/timer.ui" line="272"/>
         <source>TIMER DURATION</source>
-        <translation type="unfinished"></translation>
+        <translation>计时器持续时间</translation>
     </message>
     <message>
         <location filename="../timer/timer.ui" line="332"/>
         <source>Hmm...</source>
-        <translation type="unfinished"></translation>
+        <translation>嗯。。。</translation>
     </message>
     <message>
         <location filename="../timer/timer.ui" line="342"/>
         <source>Looks like the the24 service is not running, and timers won&apos;t work without it.</source>
-        <translation type="unfinished"></translation>
+        <translation>the24服务未运行，没有它，计时器将无法工作。</translation>
     </message>
 </context>
 <context>
@@ -297,12 +297,12 @@
     <message>
         <location filename="../timer/timerwidget.ui" line="74"/>
         <source>Reset</source>
-        <translation type="unfinished"></translation>
+        <translation>重置</translation>
     </message>
     <message>
         <location filename="../timer/timerwidget.ui" line="98"/>
         <source>Remove</source>
-        <translation type="unfinished"></translation>
+        <translation>移除</translation>
     </message>
 </context>
 <context>
@@ -310,7 +310,7 @@
     <message>
         <location filename="../worldclock/worldclock.ui" line="78"/>
         <source>Add Clock</source>
-        <translation type="unfinished"></translation>
+        <translation>添加时钟</translation>
     </message>
 </context>
 <context>
@@ -318,41 +318,41 @@
     <message>
         <location filename="../worldclock/worldclockwidget.cpp" line="67"/>
         <source>For this clock</source>
-        <translation type="unfinished"></translation>
+        <translation>对于这个时钟</translation>
     </message>
     <message>
         <location filename="../worldclock/worldclockwidget.cpp" line="68"/>
         <source>Remove</source>
-        <translation type="unfinished"></translation>
+        <translation>移除</translation>
     </message>
     <message>
         <location filename="../worldclock/worldclockwidget.cpp" line="96"/>
         <source>Current Timezone</source>
-        <translation type="unfinished"></translation>
+        <translation>当前时区</translation>
     </message>
     <message numerus="yes">
         <location filename="../worldclock/worldclockwidget.cpp" line="101"/>
         <source>%n hours behind</source>
-        <translation type="unfinished">
-            <numerusform></numerusform>
+        <translation>
+            <numerusform>落后％n小时</numerusform>
         </translation>
     </message>
     <message numerus="yes">
         <location filename="../worldclock/worldclockwidget.cpp" line="103"/>
         <source>%n hours ahead</source>
-        <translation type="unfinished">
-            <numerusform></numerusform>
+        <translation>
+            <numerusform>提前％n小时</numerusform>
         </translation>
     </message>
     <message>
         <location filename="../worldclock/worldclockwidget.cpp" line="107"/>
         <source>%1 behind</source>
-        <translation type="unfinished"></translation>
+        <translation>落后％1</translation>
     </message>
     <message>
         <location filename="../worldclock/worldclockwidget.cpp" line="109"/>
         <source>%1 ahead</source>
-        <translation type="unfinished"></translation>
+        <translation>提前％1</translation>
     </message>
 </context>
 <context>
@@ -360,7 +360,7 @@
     <message>
         <location filename="../main.cpp" line="42"/>
         <source>Clock</source>
-        <translation type="unfinished"></translation>
+        <translation>时钟</translation>
     </message>
 </context>
 </TS>
