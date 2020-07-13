8f5e7628d015aa2f33a5141f852dc6940894e2cd
Oscar <blue@bluexguardian.com>
2020-07-13 13:48:23 +0000

    Translated using Weblate (Chinese (Simplified))
    Currently translated at 100.0% (9 of 9 strings)
    
    Translation: the24/the24 Daemon
    Translate-URL: https://translate.vicr123.com/projects/the24/the24-daemon/zh_Hans/

--- daemon/translations/zh_CN.ts
+++ daemon/translations/zh_CN.ts
@@ -1,22 +1,22 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" sourcelanguage="en_US">
+<TS version="2.1" sourcelanguage="en_US" language="zh_CN">
 <context>
     <name>Alarm</name>
     <message>
         <location filename="../alarm.cpp" line="106"/>
         <source>the24</source>
-        <translation type="unfinished"></translation>
+        <translation>the24</translation>
     </message>
     <message>
         <location filename="../alarm.cpp" line="107"/>
         <source>Alarm!</source>
-        <translation type="unfinished"></translation>
+        <translation>闹铃！</translation>
     </message>
     <message>
         <location filename="../alarm.cpp" line="110"/>
         <source>Snooze</source>
-        <translation type="unfinished"></translation>
+        <translation>打盹</translation>
     </message>
 </context>
 <context>
@@ -24,17 +24,17 @@
     <message>
         <location filename="../stopwatch.cpp" line="183"/>
         <source>the24</source>
-        <translation type="unfinished"></translation>
+        <translation>the24</translation>
     </message>
     <message>
         <location filename="../stopwatch.cpp" line="184"/>
         <source>Stopwatch</source>
-        <translation type="unfinished"></translation>
+        <translation>跑表</translation>
     </message>
     <message>
         <location filename="../stopwatch.cpp" line="185"/>
         <source>A stopwatch was reset because the system clock was changed unexpectedly.</source>
-        <translation type="unfinished"></translation>
+        <translation>由于系统时钟意外更改，因此秒表已重置。</translation>
     </message>
 </context>
 <context>
@@ -42,17 +42,17 @@
     <message>
         <location filename="../timer.cpp" line="236"/>
         <source>the24</source>
-        <translation type="unfinished"></translation>
+        <translation>the24</translation>
     </message>
     <message>
         <location filename="../timer.cpp" line="237"/>
         <source>Time&apos;s up!</source>
-        <translation type="unfinished"></translation>
+        <translation>时间到！</translation>
     </message>
     <message>
         <location filename="../timer.cpp" line="239"/>
         <source>Your timer has elapsed!</source>
-        <translation type="unfinished"></translation>
+        <translation>您的计时器已过！</translation>
     </message>
 </context>
 </TS>
