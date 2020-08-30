ad5636badc24ea477788f12bb77d7a310c216cea
luca009 <realluca009@gmail.com>
2020-08-30 13:43:08 +0000

    Translated using Weblate (German)
    Currently translated at 100.0% (9 of 9 strings)
    
    Translation: the24/the24 Daemon
    Translate-URL: https://translate.vicr123.com/projects/the24/the24-daemon/de/

--- daemon/translations/de_DE.ts
+++ daemon/translations/de_DE.ts
@@ -6,17 +6,17 @@
     <message>
         <location filename="../alarm.cpp" line="106"/>
         <source>the24</source>
-        <translation type="unfinished"></translation>
+        <translation>the24</translation>
     </message>
     <message>
         <location filename="../alarm.cpp" line="107"/>
         <source>Alarm!</source>
-        <translation type="unfinished"></translation>
+        <translation>Wecker!</translation>
     </message>
     <message>
         <location filename="../alarm.cpp" line="110"/>
         <source>Snooze</source>
-        <translation type="unfinished"></translation>
+        <translation>Schlummern</translation>
     </message>
 </context>
 <context>
@@ -24,17 +24,17 @@
     <message>
         <location filename="../stopwatch.cpp" line="183"/>
         <source>the24</source>
-        <translation type="unfinished"></translation>
+        <translation>the24</translation>
     </message>
     <message>
         <location filename="../stopwatch.cpp" line="184"/>
         <source>Stopwatch</source>
-        <translation type="unfinished"></translation>
+        <translation>Stoppuhr</translation>
     </message>
     <message>
         <location filename="../stopwatch.cpp" line="185"/>
         <source>A stopwatch was reset because the system clock was changed unexpectedly.</source>
-        <translation type="unfinished"></translation>
+        <translation>Eine Stoppuhr wurde zurückgesetzt, da die System-Uhr unerwartet geändert wurde.</translation>
     </message>
 </context>
 <context>
@@ -42,17 +42,17 @@
     <message>
         <location filename="../timer.cpp" line="236"/>
         <source>the24</source>
-        <translation type="unfinished"></translation>
+        <translation>the24</translation>
     </message>
     <message>
         <location filename="../timer.cpp" line="237"/>
         <source>Time&apos;s up!</source>
-        <translation type="unfinished"></translation>
+        <translation>Die Zeit ist um!</translation>
     </message>
     <message>
         <location filename="../timer.cpp" line="239"/>
         <source>Your timer has elapsed!</source>
-        <translation type="unfinished"></translation>
+        <translation>Dein Timer ist vergangen!</translation>
     </message>
 </context>
 </TS>
