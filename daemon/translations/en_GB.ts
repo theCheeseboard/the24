b47ecd440736a53a0a3c5d37857426bf189bfb68
Atahan Reis <atahanreis@icloud.com>
2020-12-24 16:10:22 +0000

    Translated using Weblate (English (United Kingdom))
    Currently translated at 100.0% (9 of 9 strings)
    
    Translation: the24/the24 Daemon
    Translate-URL: https://translate.vicr123.com/projects/the24/the24-daemon/en_GB/

--- daemon/translations/en_GB.ts
+++ daemon/translations/en_GB.ts
@@ -1,22 +1,22 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" sourcelanguage="en_US">
+<TS version="2.1" sourcelanguage="en_US" language="en_GB">
 <context>
     <name>Alarm</name>
     <message>
         <location filename="../alarm.cpp" line="106"/>
         <source>the24</source>
-        <translation type="unfinished"></translation>
+        <translation>the24</translation>
     </message>
     <message>
         <location filename="../alarm.cpp" line="107"/>
         <source>Alarm!</source>
-        <translation type="unfinished"></translation>
+        <translation>Alarm!</translation>
     </message>
     <message>
         <location filename="../alarm.cpp" line="110"/>
         <source>Snooze</source>
-        <translation type="unfinished"></translation>
+        <translation>Snooze</translation>
     </message>
 </context>
 <context>
@@ -24,17 +24,17 @@
     <message>
         <location filename="../stopwatch.cpp" line="183"/>
         <source>the24</source>
-        <translation type="unfinished"></translation>
+        <translation>the24</translation>
     </message>
     <message>
         <location filename="../stopwatch.cpp" line="184"/>
         <source>Stopwatch</source>
-        <translation type="unfinished"></translation>
+        <translation>Stopwatch</translation>
     </message>
     <message>
         <location filename="../stopwatch.cpp" line="185"/>
         <source>A stopwatch was reset because the system clock was changed unexpectedly.</source>
-        <translation type="unfinished"></translation>
+        <translation>A stopwatch was reset because the system clock was changed unexpectedly.</translation>
     </message>
 </context>
 <context>
@@ -42,17 +42,17 @@
     <message>
         <location filename="../timer.cpp" line="236"/>
         <source>the24</source>
-        <translation type="unfinished"></translation>
+        <translation>the24</translation>
     </message>
     <message>
         <location filename="../timer.cpp" line="237"/>
         <source>Time&apos;s up!</source>
-        <translation type="unfinished"></translation>
+        <translation>Time&apos;s up!</translation>
     </message>
     <message>
         <location filename="../timer.cpp" line="239"/>
         <source>Your timer has elapsed!</source>
-        <translation type="unfinished"></translation>
+        <translation>Your timer has elapsed!</translation>
     </message>
 </context>
 </TS>
