ba6c9c9fb8eb6e4f339a9ffb03108a1eba109ad0
Victor Tran <vicr12345@gmail.com>
2022-11-05 18:45:32 +1100

    Add base translation files

---
+++ daemon/translations/en_US.ts
@@ -0,0 +1,58 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US" sourcelanguage="en_US">
+<context>
+    <name>Alarm</name>
+    <message>
+        <location filename="../alarm.cpp" line="107"/>
+        <source>the24</source>
+        <translation>the24</translation>
+    </message>
+    <message>
+        <location filename="../alarm.cpp" line="108"/>
+        <source>Alarm!</source>
+        <translation>Alarm!</translation>
+    </message>
+    <message>
+        <location filename="../alarm.cpp" line="111"/>
+        <source>Snooze</source>
+        <translation>Snooze</translation>
+    </message>
+</context>
+<context>
+    <name>Stopwatch</name>
+    <message>
+        <location filename="../stopwatch.cpp" line="184"/>
+        <source>the24</source>
+        <translation>the24</translation>
+    </message>
+    <message>
+        <location filename="../stopwatch.cpp" line="185"/>
+        <source>Stopwatch</source>
+        <translation>Stopwatch</translation>
+    </message>
+    <message>
+        <location filename="../stopwatch.cpp" line="186"/>
+        <source>A stopwatch was reset because the system clock was changed unexpectedly.</source>
+        <translation>A stopwatch was reset because the system clock was changed unexpectedly.</translation>
+    </message>
+</context>
+<context>
+    <name>Timer</name>
+    <message>
+        <location filename="../timer.cpp" line="237"/>
+        <source>the24</source>
+        <translation>the24</translation>
+    </message>
+    <message>
+        <location filename="../timer.cpp" line="238"/>
+        <source>Time&apos;s up!</source>
+        <translation>Time&apos;s up!</translation>
+    </message>
+    <message>
+        <location filename="../timer.cpp" line="240"/>
+        <source>Your timer has elapsed!</source>
+        <translation>Your timer has elapsed!</translation>
+    </message>
+</context>
+</TS>
