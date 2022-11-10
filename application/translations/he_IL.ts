179200e32b8114cbd05b4fcded30938a808e0980
Parlance <parlance@vicr123.com>
2022-11-10 23:35:36 +0000

    Update Translations - he_IL
    Co-Authored-By: martysh12 <martyshkon@gmail.com>

---
+++ application/translations/he_IL.ts
@@ -0,0 +1,362 @@
+﻿<?xml version="1.0" encoding="utf-8"?>
+<TS version="2.1" language="he_IL">
+  <context>
+    <name>AddClockPopover</name>
+    <message>
+      <translation>הוסף שעון</translation>
+      <location filename="../worldclock/addclockpopover.ui" line="35" />
+      <source>Add Clock</source>
+    </message>
+    <message>
+      <translation>איזה אזור זמן ברצונך להוסיף?</translation>
+      <location filename="../worldclock/addclockpopover.ui" line="42" />
+      <source>What timezone do you want to add?</source>
+    </message>
+    <message>
+      <translation>חיפוש</translation>
+      <location filename="../worldclock/addclockpopover.ui" line="71" />
+      <source>Search</source>
+    </message>
+  </context>
+  <context>
+    <name>AlarmPopover</name>
+    <message>
+      <translation>חזור ב-</translation>
+      <location filename="../alarm/alarmpopover.ui" line="97" />
+      <source>Repeat on</source>
+    </message>
+    <message>
+      <translation>הוסף שעון מעורר</translation>
+      <location filename="../alarm/alarmpopover.ui" line="135" />
+      <source>Add Alarm</source>
+    </message>
+    <message>
+      <translation>ערוך שעון מעורר</translation>
+      <location filename="../alarm/alarmpopover.cpp" line="47" />
+      <source>Edit Alarm</source>
+    </message>
+    <message>
+      <translation>שעון מעורר חדש</translation>
+      <location filename="../alarm/alarmpopover.cpp" line="49" />
+      <source>New Alarm</source>
+    </message>
+  </context>
+  <context>
+    <name>AlarmWidget</name>
+    <message>
+      <translation>חוזר כל יום</translation>
+      <location filename="../alarm/alarmwidget.cpp" line="111" />
+      <source>Repeats every day</source>
+    </message>
+    <message>
+      <translation>חוזר בימי חול</translation>
+      <location filename="../alarm/alarmwidget.cpp" line="114" />
+      <source>Repeats on Weekdays</source>
+    </message>
+    <message>
+      <translation>חוזר בסופי שבוע</translation>
+      <location filename="../alarm/alarmwidget.cpp" line="117" />
+      <source>Repeats on Weekends</source>
+    </message>
+    <message>
+      <translation>חוזר ב-%1</translation>
+      <location filename="../alarm/alarmwidget.cpp" line="132" />
+      <source>Repeats on %1</source>
+    </message>
+    <message>
+      <translation>הושהה עד %1</translation>
+      <location filename="../alarm/alarmwidget.cpp" line="146" />
+      <source>Snoozed until %1</source>
+    </message>
+    <message>
+      <translation>עבור שעון מעורר זה</translation>
+      <location filename="../alarm/alarmwidget.cpp" line="160" />
+      <source>For this alarm</source>
+    </message>
+    <message>
+      <translation>ערוך</translation>
+      <location filename="../alarm/alarmwidget.cpp" line="161" />
+      <source>Edit</source>
+    </message>
+    <message>
+      <translation>הסר</translation>
+      <location filename="../alarm/alarmwidget.cpp" line="170" />
+      <source>Remove</source>
+    </message>
+  </context>
+  <context>
+    <name>Alarms</name>
+    <message>
+      <translation>הוסף שעון מעורר</translation>
+      <location filename="../alarm/alarms.ui" line="100" />
+      <source>Add Alarm</source>
+    </message>
+    <message>
+      <translation>הממ...</translation>
+      <location filename="../alarm/alarms.ui" line="153" />
+      <source>Hmm...</source>
+    </message>
+    <message>
+      <translation>נראה ששירות the24 לא פועל, ושעונים מעוררים לא יעבדו בלעדיו.</translation>
+      <location filename="../alarm/alarms.ui" line="163" />
+      <source>Looks like the the24 service is not running, and alarms won't work without it.</source>
+    </message>
+  </context>
+  <context>
+    <name>ClockHelpers</name>
+    <message>
+      <translation>יום</translation>
+      <location filename="../clockhelpers.cpp" line="38" />
+      <source>d</source>
+    </message>
+  </context>
+  <context>
+    <name>MainWindow</name>
+    <message>
+      <translation>the24</translation>
+      <location filename="../mainwindow.ui" line="14" />
+      <location filename="../../../build-the24-Chroot_6-Debug/application/the24_autogen/include/ui_mainwindow.h" line="147" />
+      <source>the24</source>
+    </message>
+    <message>
+      <translation>שעון עולמי</translation>
+      <location filename="../mainwindow.cpp" line="55" />
+      <source>World Clock</source>
+    </message>
+    <message>
+      <translation>שעונים מעוררים</translation>
+      <location filename="../mainwindow.cpp" line="56" />
+      <source>Alarms</source>
+    </message>
+    <message>
+      <translation>שעון עצר</translation>
+      <location filename="../mainwindow.cpp" line="57" />
+      <source>Stopwatch</source>
+    </message>
+    <message>
+      <translation>טימר</translation>
+      <location filename="../mainwindow.cpp" line="58" />
+      <source>Timer</source>
+    </message>
+    <message>
+      <translation>דווח על באג</translation>
+      <source>File Bug</source>
+    </message>
+    <message>
+      <translation>מקורות</translation>
+      <source>Sources</source>
+    </message>
+    <message>
+      <translation>מידע על</translation>
+      <source>About</source>
+    </message>
+    <message>
+      <translation>יציאה</translation>
+      <location filename="../mainwindow.ui" line="121" />
+      <location filename="../../../build-the24-Chroot_6-Debug/application/the24_autogen/include/ui_mainwindow.h" line="148" />
+      <source>Exit</source>
+    </message>
+    <message>
+      <translation>Ctrl+Q</translation>
+      <location filename="../mainwindow.ui" line="124" />
+      <location filename="../../../build-the24-Chroot_6-Debug/application/the24_autogen/include/ui_mainwindow.h" line="150" />
+      <source>Ctrl+Q</source>
+    </message>
+    <message>
+      <translation>עזרה</translation>
+      <source>Help</source>
+    </message>
+  </context>
+  <context>
+    <name>NewTimerPopover</name>
+    <message>
+      <translation>הוסף טימר</translation>
+      <location filename="../timer/newtimerpopover.ui" line="35" />
+      <source>Add Timer</source>
+    </message>
+    <message>
+      <translation>הפעל טימר</translation>
+      <location filename="../timer/newtimerpopover.ui" line="45" />
+      <source>Start Timer</source>
+    </message>
+  </context>
+  <context>
+    <name>Stopwatch</name>
+    <message>
+      <translation>הפעל שעון עצר</translation>
+      <location filename="../stopwatch/stopwatch.ui" line="100" />
+      <source>Start Stopwatch</source>
+    </message>
+    <message>
+      <translation>הממ...</translation>
+      <location filename="../stopwatch/stopwatch.ui" line="153" />
+      <source>Hmm...</source>
+    </message>
+    <message>
+      <translation>נראה ששירות the24 לא פועל, ושעוני עצר לא יעבדו בלעדיו.</translation>
+      <location filename="../stopwatch/stopwatch.ui" line="163" />
+      <source>Looks like the the24 service is not running, and stopwatches won't work without it.</source>
+    </message>
+  </context>
+  <context>
+    <name>StopwatchWidget</name>
+    <message>
+      <translation>אפס</translation>
+      <location filename="../stopwatch/stopwatchwidget.ui" line="74" />
+      <source>Reset</source>
+    </message>
+    <message>
+      <translation>הסר</translation>
+      <location filename="../stopwatch/stopwatchwidget.ui" line="98" />
+      <source>Remove</source>
+    </message>
+  </context>
+  <context>
+    <name>Timer</name>
+    <message>
+      <translation>הוסף טימר</translation>
+      <location filename="../timer/timer.ui" line="100" />
+      <source>Add Timer</source>
+    </message>
+    <message>
+      <translation>הפעל טימר</translation>
+      <location filename="../timer/timer.ui" line="156" />
+      <source>Start a timer</source>
+    </message>
+    <message>
+      <translation>15 דק'</translation>
+      <location filename="../timer/timer.ui" line="203" />
+      <source>15 min</source>
+    </message>
+    <message>
+      <translation>2 שעות</translation>
+      <location filename="../timer/timer.ui" line="210" />
+      <source>2 hr</source>
+    </message>
+    <message>
+      <translation>שעה 1</translation>
+      <location filename="../timer/timer.ui" line="224" />
+      <source>1 hr</source>
+    </message>
+    <message>
+      <translation>דקה 1</translation>
+      <location filename="../timer/timer.ui" line="231" />
+      <source>1 min</source>
+    </message>
+    <message>
+      <translation>2 דק'</translation>
+      <location filename="../timer/timer.ui" line="238" />
+      <source>2 min</source>
+    </message>
+    <message>
+      <translation>30 דק'</translation>
+      <location filename="../timer/timer.ui" line="245" />
+      <source>30 min</source>
+    </message>
+    <message>
+      <translation>5 דק'</translation>
+      <location filename="../timer/timer.ui" line="252" />
+      <source>5 min</source>
+    </message>
+    <message>
+      <translation>10 דק'</translation>
+      <location filename="../timer/timer.ui" line="259" />
+      <source>10 min</source>
+    </message>
+    <message>
+      <translation>משהו אחר</translation>
+      <location filename="../timer/timer.ui" line="217" />
+      <source>Something Else</source>
+    </message>
+    <message>
+      <translation>משך טימר</translation>
+      <location filename="../timer/timer.ui" line="272" />
+      <source>TIMER DURATION</source>
+    </message>
+    <message>
+      <translation>הממ...</translation>
+      <location filename="../timer/timer.ui" line="332" />
+      <source>Hmm...</source>
+    </message>
+    <message>
+      <translation>נראה ששירות the24 לא פועל, וטימרים לא יעבדו בלעדיו.</translation>
+      <location filename="../timer/timer.ui" line="342" />
+      <source>Looks like the the24 service is not running, and timers won't work without it.</source>
+    </message>
+  </context>
+  <context>
+    <name>TimerWidget</name>
+    <message>
+      <translation>אפס</translation>
+      <location filename="../timer/timerwidget.ui" line="74" />
+      <source>Reset</source>
+    </message>
+    <message>
+      <translation>הסר</translation>
+      <location filename="../timer/timerwidget.ui" line="98" />
+      <source>Remove</source>
+    </message>
+  </context>
+  <context>
+    <name>WorldClock</name>
+    <message>
+      <translation>הוסף שעון</translation>
+      <location filename="../worldclock/worldclock.ui" line="78" />
+      <source>Add Clock</source>
+    </message>
+  </context>
+  <context>
+    <name>WorldClockWidget</name>
+    <message>
+      <translation>עבור שעון זה</translation>
+      <location filename="../worldclock/worldclockwidget.cpp" line="67" />
+      <source>For this clock</source>
+    </message>
+    <message>
+      <translation>הסר</translation>
+      <location filename="../worldclock/worldclockwidget.cpp" line="68" />
+      <source>Remove</source>
+    </message>
+    <message>
+      <translation>אזור זמן נוכחי</translation>
+      <location filename="../worldclock/worldclockwidget.cpp" line="96" />
+      <source>Current Timezone</source>
+    </message>
+    <message numerus="yes">
+      <translation>
+        <numerusform>שעה %n מאחור</numerusform>
+        <numerusform>%n שעות מאחור</numerusform>
+        <numerusform>%n שעות מאחור</numerusform>
+      </translation>
+      <location filename="../worldclock/worldclockwidget.cpp" line="101" />
+      <source>%n hours behind</source>
+    </message>
+    <message numerus="yes">
+      <translation>
+        <numerusform>שעה %n קדימה</numerusform>
+        <numerusform>%n שעות קדימה</numerusform>
+        <numerusform>%n שעות קדימה</numerusform>
+      </translation>
+      <location filename="../worldclock/worldclockwidget.cpp" line="103" />
+      <source>%n hours ahead</source>
+    </message>
+    <message>
+      <translation>%1 מאחור</translation>
+      <location filename="../worldclock/worldclockwidget.cpp" line="107" />
+      <source>%1 behind</source>
+    </message>
+    <message>
+      <translation>%1 קדימה</translation>
+      <location filename="../worldclock/worldclockwidget.cpp" line="109" />
+      <source>%1 ahead</source>
+    </message>
+  </context>
+  <context>
+    <name>main</name>
+    <message>
+      <translation>שעון</translation>
+      <location filename="../main.cpp" line="37" />
+      <source>Clock</source>
+    </message>
+  </context>
+</TS>
---
+++ daemon/translations/he_IL.ts
@@ -0,0 +1,57 @@
+﻿<?xml version="1.0" encoding="utf-8"?>
+<TS version="2.1" language="he_IL">
+  <context>
+    <name>Alarm</name>
+    <message>
+      <translation>the24</translation>
+      <location filename="../alarm.cpp" line="107" />
+      <source>the24</source>
+    </message>
+    <message>
+      <translation>שעון מעורר!</translation>
+      <location filename="../alarm.cpp" line="108" />
+      <source>Alarm!</source>
+    </message>
+    <message>
+      <translation>השהה</translation>
+      <location filename="../alarm.cpp" line="111" />
+      <source>Snooze</source>
+    </message>
+  </context>
+  <context>
+    <name>Stopwatch</name>
+    <message>
+      <translation>the24</translation>
+      <location filename="../stopwatch.cpp" line="184" />
+      <source>the24</source>
+    </message>
+    <message>
+      <translation>שעון עצר</translation>
+      <location filename="../stopwatch.cpp" line="185" />
+      <source>Stopwatch</source>
+    </message>
+    <message>
+      <translation>שעון עצר אופס בגלל ששעון המערכת שונה באופן בלתי צפוי.</translation>
+      <location filename="../stopwatch.cpp" line="186" />
+      <source>A stopwatch was reset because the system clock was changed unexpectedly.</source>
+    </message>
+  </context>
+  <context>
+    <name>Timer</name>
+    <message>
+      <translation>the24</translation>
+      <location filename="../timer.cpp" line="237" />
+      <source>the24</source>
+    </message>
+    <message>
+      <translation>הזמן נגמר!</translation>
+      <location filename="../timer.cpp" line="238" />
+      <source>Time's up!</source>
+    </message>
+    <message>
+      <translation>הטימר שלך חלף!</translation>
+      <location filename="../timer.cpp" line="240" />
+      <source>Your timer has elapsed!</source>
+    </message>
+  </context>
+</TS>
