5f1b35a2ee959c92734171eb09500e3f4ee86d5c
Parlance <parlance@vicr123.com>
2024-09-25 14:23:47 +0000

    Update Translations

---
+++ application/translations/it.ts
@@ -0,0 +1,360 @@
+﻿<?xml version="1.0" encoding="utf-8"?>
+<TS version="2.1" language="it">
+  <context>
+    <name>AddClockPopover</name>
+    <message>
+      <translation></translation>
+      <location filename="../worldclock/addclockpopover.ui" line="35" />
+      <source>Add Clock</source>
+    </message>
+    <message>
+      <translation></translation>
+      <location filename="../worldclock/addclockpopover.ui" line="42" />
+      <source>What timezone do you want to add?</source>
+    </message>
+    <message>
+      <translation></translation>
+      <location filename="../worldclock/addclockpopover.ui" line="71" />
+      <source>Search</source>
+    </message>
+  </context>
+  <context>
+    <name>AlarmPopover</name>
+    <message>
+      <translation></translation>
+      <location filename="../alarm/alarmpopover.ui" line="97" />
+      <source>Repeat on</source>
+    </message>
+    <message>
+      <translation></translation>
+      <location filename="../alarm/alarmpopover.ui" line="135" />
+      <source>Add Alarm</source>
+    </message>
+    <message>
+      <translation></translation>
+      <location filename="../alarm/alarmpopover.cpp" line="47" />
+      <source>Edit Alarm</source>
+    </message>
+    <message>
+      <translation></translation>
+      <location filename="../alarm/alarmpopover.cpp" line="49" />
+      <source>New Alarm</source>
+    </message>
+  </context>
+  <context>
+    <name>AlarmWidget</name>
+    <message>
+      <translation></translation>
+      <location filename="../alarm/alarmwidget.cpp" line="111" />
+      <source>Repeats every day</source>
+    </message>
+    <message>
+      <translation></translation>
+      <location filename="../alarm/alarmwidget.cpp" line="114" />
+      <source>Repeats on Weekdays</source>
+    </message>
+    <message>
+      <translation></translation>
+      <location filename="../alarm/alarmwidget.cpp" line="117" />
+      <source>Repeats on Weekends</source>
+    </message>
+    <message>
+      <translation></translation>
+      <location filename="../alarm/alarmwidget.cpp" line="132" />
+      <source>Repeats on %1</source>
+    </message>
+    <message>
+      <translation></translation>
+      <location filename="../alarm/alarmwidget.cpp" line="146" />
+      <source>Snoozed until %1</source>
+    </message>
+    <message>
+      <translation></translation>
+      <location filename="../alarm/alarmwidget.cpp" line="160" />
+      <source>For this alarm</source>
+    </message>
+    <message>
+      <translation></translation>
+      <location filename="../alarm/alarmwidget.cpp" line="161" />
+      <source>Edit</source>
+    </message>
+    <message>
+      <translation></translation>
+      <location filename="../alarm/alarmwidget.cpp" line="170" />
+      <source>Remove</source>
+    </message>
+  </context>
+  <context>
+    <name>Alarms</name>
+    <message>
+      <translation></translation>
+      <location filename="../alarm/alarms.ui" line="100" />
+      <source>Add Alarm</source>
+    </message>
+    <message>
+      <translation></translation>
+      <location filename="../alarm/alarms.ui" line="153" />
+      <source>Hmm...</source>
+    </message>
+    <message>
+      <translation></translation>
+      <location filename="../alarm/alarms.ui" line="163" />
+      <source>Looks like the the24 service is not running, and alarms won't work without it.</source>
+    </message>
+  </context>
+  <context>
+    <name>ClockHelpers</name>
+    <message>
+      <translation></translation>
+      <location filename="../clockhelpers.cpp" line="38" />
+      <source>d</source>
+    </message>
+  </context>
+  <context>
+    <name>MainWindow</name>
+    <message>
+      <translation></translation>
+      <location filename="../mainwindow.ui" line="14" />
+      <location filename="../../../build-the24-Chroot_6-Debug/application/the24_autogen/include/ui_mainwindow.h" line="146" />
+      <source>the24</source>
+    </message>
+    <message>
+      <translation></translation>
+      <location filename="../mainwindow.cpp" line="59" />
+      <source>World Clock</source>
+    </message>
+    <message>
+      <translation></translation>
+      <location filename="../mainwindow.cpp" line="60" />
+      <source>Alarms</source>
+    </message>
+    <message>
+      <translation></translation>
+      <location filename="../mainwindow.cpp" line="61" />
+      <source>Stopwatch</source>
+    </message>
+    <message>
+      <translation></translation>
+      <location filename="../mainwindow.cpp" line="62" />
+      <source>Timer</source>
+    </message>
+    <message>
+      <translation></translation>
+      <source>File Bug</source>
+    </message>
+    <message>
+      <translation></translation>
+      <source>Sources</source>
+    </message>
+    <message>
+      <translation></translation>
+      <source>About</source>
+    </message>
+    <message>
+      <translation></translation>
+      <location filename="../mainwindow.ui" line="117" />
+      <location filename="../../../build-the24-Chroot_6-Debug/application/the24_autogen/include/ui_mainwindow.h" line="147" />
+      <source>Exit</source>
+    </message>
+    <message>
+      <translation></translation>
+      <location filename="../mainwindow.ui" line="120" />
+      <location filename="../../../build-the24-Chroot_6-Debug/application/the24_autogen/include/ui_mainwindow.h" line="149" />
+      <source>Ctrl+Q</source>
+    </message>
+    <message>
+      <translation></translation>
+      <source>Help</source>
+    </message>
+  </context>
+  <context>
+    <name>NewTimerPopover</name>
+    <message>
+      <translation></translation>
+      <location filename="../timer/newtimerpopover.ui" line="35" />
+      <source>Add Timer</source>
+    </message>
+    <message>
+      <translation></translation>
+      <location filename="../timer/newtimerpopover.ui" line="45" />
+      <source>Start Timer</source>
+    </message>
+  </context>
+  <context>
+    <name>Stopwatch</name>
+    <message>
+      <translation></translation>
+      <location filename="../stopwatch/stopwatch.ui" line="100" />
+      <source>Start Stopwatch</source>
+    </message>
+    <message>
+      <translation></translation>
+      <location filename="../stopwatch/stopwatch.ui" line="153" />
+      <source>Hmm...</source>
+    </message>
+    <message>
+      <translation></translation>
+      <location filename="../stopwatch/stopwatch.ui" line="163" />
+      <source>Looks like the the24 service is not running, and stopwatches won't work without it.</source>
+    </message>
+  </context>
+  <context>
+    <name>StopwatchWidget</name>
+    <message>
+      <translation></translation>
+      <location filename="../stopwatch/stopwatchwidget.ui" line="74" />
+      <source>Reset</source>
+    </message>
+    <message>
+      <translation></translation>
+      <location filename="../stopwatch/stopwatchwidget.ui" line="98" />
+      <source>Remove</source>
+    </message>
+  </context>
+  <context>
+    <name>Timer</name>
+    <message>
+      <translation></translation>
+      <location filename="../timer/timer.ui" line="100" />
+      <source>Add Timer</source>
+    </message>
+    <message>
+      <translation></translation>
+      <location filename="../timer/timer.ui" line="156" />
+      <source>Start a timer</source>
+    </message>
+    <message>
+      <translation></translation>
+      <location filename="../timer/timer.ui" line="203" />
+      <source>15 min</source>
+    </message>
+    <message>
+      <translation></translation>
+      <location filename="../timer/timer.ui" line="210" />
+      <source>2 hr</source>
+    </message>
+    <message>
+      <translation></translation>
+      <location filename="../timer/timer.ui" line="224" />
+      <source>1 hr</source>
+    </message>
+    <message>
+      <translation></translation>
+      <location filename="../timer/timer.ui" line="231" />
+      <source>1 min</source>
+    </message>
+    <message>
+      <translation></translation>
+      <location filename="../timer/timer.ui" line="238" />
+      <source>2 min</source>
+    </message>
+    <message>
+      <translation></translation>
+      <location filename="../timer/timer.ui" line="245" />
+      <source>30 min</source>
+    </message>
+    <message>
+      <translation></translation>
+      <location filename="../timer/timer.ui" line="252" />
+      <source>5 min</source>
+    </message>
+    <message>
+      <translation></translation>
+      <location filename="../timer/timer.ui" line="259" />
+      <source>10 min</source>
+    </message>
+    <message>
+      <translation></translation>
+      <location filename="../timer/timer.ui" line="217" />
+      <source>Something Else</source>
+    </message>
+    <message>
+      <translation></translation>
+      <location filename="../timer/timer.ui" line="272" />
+      <source>TIMER DURATION</source>
+    </message>
+    <message>
+      <translation></translation>
+      <location filename="../timer/timer.ui" line="332" />
+      <source>Hmm...</source>
+    </message>
+    <message>
+      <translation></translation>
+      <location filename="../timer/timer.ui" line="342" />
+      <source>Looks like the the24 service is not running, and timers won't work without it.</source>
+    </message>
+  </context>
+  <context>
+    <name>TimerWidget</name>
+    <message>
+      <translation></translation>
+      <location filename="../timer/timerwidget.ui" line="74" />
+      <source>Reset</source>
+    </message>
+    <message>
+      <translation></translation>
+      <location filename="../timer/timerwidget.ui" line="98" />
+      <source>Remove</source>
+    </message>
+  </context>
+  <context>
+    <name>WorldClock</name>
+    <message>
+      <translation></translation>
+      <location filename="../worldclock/worldclock.ui" line="78" />
+      <source>Add Clock</source>
+    </message>
+  </context>
+  <context>
+    <name>WorldClockWidget</name>
+    <message>
+      <translation></translation>
+      <location filename="../worldclock/worldclockwidget.cpp" line="67" />
+      <source>For this clock</source>
+    </message>
+    <message>
+      <translation></translation>
+      <location filename="../worldclock/worldclockwidget.cpp" line="68" />
+      <source>Remove</source>
+    </message>
+    <message>
+      <translation></translation>
+      <location filename="../worldclock/worldclockwidget.cpp" line="96" />
+      <source>Current Timezone</source>
+    </message>
+    <message numerus="yes">
+      <translation>
+        <numerusform></numerusform>
+        <numerusform></numerusform>
+      </translation>
+      <location filename="../worldclock/worldclockwidget.cpp" line="101" />
+      <source>%n hours behind</source>
+    </message>
+    <message numerus="yes">
+      <translation>
+        <numerusform></numerusform>
+        <numerusform></numerusform>
+      </translation>
+      <location filename="../worldclock/worldclockwidget.cpp" line="103" />
+      <source>%n hours ahead</source>
+    </message>
+    <message>
+      <translation></translation>
+      <location filename="../worldclock/worldclockwidget.cpp" line="107" />
+      <source>%1 behind</source>
+    </message>
+    <message>
+      <translation></translation>
+      <location filename="../worldclock/worldclockwidget.cpp" line="109" />
+      <source>%1 ahead</source>
+    </message>
+  </context>
+  <context>
+    <name>main</name>
+    <message>
+      <translation></translation>
+      <location filename="../main.cpp" line="36" />
+      <source>Clock</source>
+    </message>
+  </context>
+</TS>
